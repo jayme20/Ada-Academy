953b3a1b1ae07bf9dd2d9b10950e515d6b78b3d3
jayme20 <dwightjames05@gmail.com>
2021-10-22 14:39:12 +0300

    first commit

---
+++ completion_progress/amd/src/chart_renderer.js
@@ -0,0 +1,8 @@
+define(['jquery'], function($) {
+    var t = {
+        drawChart: function(dataset) {
+            var Completion_data = $.parseJSON(dataset);
+        }
+    }
+    return t;
+})
---
+++ theme/adanian/renderers.php
@@ -0,0 +1,6 @@
+<?php
+
+class theme_overridetest_core_renderer extends core_renderer
+{
+    
+}
